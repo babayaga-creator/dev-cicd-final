05c63a3b113e715d1f029e9a1d617f4186b68b6d
Arif00 <arif.cihangir@yh.nackademin.se>
2022-11-23 00:50:24 +0100

    azure aks conf

--- scripts/db.sh
+++ scripts/db.sh
@@ -16,3 +16,5 @@ docker run \
 
 kubectl create secret generic mysql-secret --from-literal=mysql-root-password=kube1234 --from-literal=mysql-user=username --from-literal=mysql-password=password --from-literal=mysql-database=example
 kubectl create configmap db --from-literal=mysql-database: database
+
+kubectl create secret docker-registry regcred --docker-server=ghcr.io --docker-username=babayaga-creator --docker-password=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  --docker-email=arif.cihangir@gmail.com
