2e2310d28ff18640a2ced48e79a2b1e454d18b49
Arif00 <arif.cihangir@yh.nackademin.se>
2022-11-24 14:21:12 +0100

    remove secret

--- scripts/db.sh
+++ scripts/db.sh
@@ -12,9 +12,3 @@ docker run \
     -e MYSQL_PASSWORD=password \
     -v "$(pwd)"/sql:/docker-entrypoint-initdb.d \
     mysql:8
-
-
-kubectl create secret generic mysql-secret --from-literal=mysql-root-password=kube1234 --from-literal=mysql-user=username --from-literal=mysql-password=password --from-literal=mysql-database=example
-kubectl create configmap db --from-literal=mysql-database: database
-
-kubectl create secret docker-registry regcred --docker-server=ghcr.io --docker-username=babayaga-creator --docker-password=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  --docker-email=arif.cihangir@gmail.com
