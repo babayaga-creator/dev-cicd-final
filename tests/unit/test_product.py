cd5575e0c758731bbdfccd1b0df24f826a5d3cd5
Arif00 <arif.cihangir@yh.nackademin.se>
2022-11-28 12:40:25 +0100

    Final Version

--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -3,4 +3,4 @@ from shop_app import product
 
 def test_get_section_message():
 
-    assert product.get_section_message() == "Here is all the registered names"
+    assert product.get_section_message() == "Here is all the registered names!"
